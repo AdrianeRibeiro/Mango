bc91c82c789b8a69f61ade74ba228f21d0582cfd
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-02-16 09:10:51 -0300

    primeiro arquivo de teste - facebook autentication

---
+++ tests/data/services/facebook-authentication.spec.ts
@@ -0,0 +1,35 @@
+import { LoadFacebookUserApi } from "@/data/contracts/apis"
+import { FacebookAuthenticationService } from "@/data/services"
+import { AuthenticationError } from "@/domain/errors"
+
+
+class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
+  token?: string
+  result = undefined
+
+  async loadUser(params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
+    this.token = params.token
+    return this.result
+  }
+}
+
+describe('FacebookAuthenticationService', () => {
+  it('should call LoadFacebookUserApi with correct params', async () => {
+    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy()
+    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
+
+    await sut.perform({ token: 'any_token' })
+
+    expect(loadFacebookUserApiSpy.token).toBe('any_token')
+  })
+
+  it('should return AuthenticationError when LoadFacebookUserApi return undefined', async () => {
+    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy()
+    loadFacebookUserApiSpy.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthenticationError())
+  })
+})
