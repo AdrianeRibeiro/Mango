545754180ffe8645b1486a0a3741f6938eb4ec11
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-02-22 08:16:51 -0300

    movendo logica de negocio para o domain

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -14,6 +14,13 @@ export class FacebookAuthenticationService {
 
     if(fbData != undefined) {
       const accountData = await this.userAccountRepo.load({ email: fbData.email })
+      const userAccount = new UserAccount({
+        id: accountData?.id,
+        name: accountData?.name,
+        email: fbData.email,
+      })
+      userAccount.updateWithFacebook(fbData)
+
       await this.userAccountRepo.saveWithFacebook({
         id: accountData?.id,
         name: accountData?.name ?? fbData.name,
@@ -23,5 +30,25 @@ export class FacebookAuthenticationService {
     }
 
     return new AuthenticationError()
+  }
+}
+
+
+class UserAccount {
+  id?: string
+  name?: string
+  email: string
+  facebookId?: string
+
+  constructor(model: { id?: string, name?: string, email: string }) {
+    this.id = model.id
+    this.name = model.name
+    this.email = model.email
+  }
+
+  updateWithFacebook(model: { name: string, email: string, facebookId: string }): void {
+    this.email = model.email
+    this.facebookId = model.facebookId
+    this.name = this.name ?? model.name
   }
 }
