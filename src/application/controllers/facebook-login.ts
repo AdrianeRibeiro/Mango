db290420359646cc31ff6e575871101379d47c39
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-03-17 08:56:57 -0300

    usar required string validator no controller facebook-login

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,7 @@
 import { FacebookAuthentication } from "@/domain/features"
 import { HttpResponse, badRequest, ok, serverError, unauthorized } from "@/application/helpers"
 import { AccessToken } from "@/domain/models"
-import { RequiredFieldError, ServerError } from "@/application/errors"
+import { RequiredStringValidator } from "@/application/validation"
 
 type httpRequest = {
   token: string
@@ -32,8 +32,7 @@ export class FacebookLoginController {
   }
 
   private validate(httpRequest: httpRequest): Error | undefined {
-    if(httpRequest.token == '' || httpRequest.token == null || httpRequest.token == undefined) {
-      return new RequiredFieldError('token')
-    }
+    const validator = new RequiredStringValidator(httpRequest.token, 'token')
+    return validator.validate()
   }
 }
