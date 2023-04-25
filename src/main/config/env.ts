712caab2f2b91b7a3ef5c1b53cf3a8ee1a7e3773
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-04-25 08:59:05 -0300

    facebook login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,6 +3,7 @@ export const env = {
     clientId: process.env.FB_CLIENT_ID ?? '254685603576440',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '170d92af8f36662bb28b6126643156f9'
   },
-  port: process.env.PORT ?? 8080
+  port: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWTSECRET ?? 'asdfg123'
 }
 
---
+++ src/main/factories/services/facebook-authentication.ts
@@ -0,0 +1,15 @@
+import { FacebookAuthenticationService } from "@/data/services"
+import { env } from "@/main/config/env"
+import { JwtTokenGenerator } from "@/infra/crypto"
+import { makeFacebookApi } from "@/main/factories/apis"
+import { makePgUserAccountRepo } from "@/main/factories/repos"
+import { makeJwtTokenGenerator } from "@/main/factories/crypto"
+
+export const makeFacebookAuthenticationService = (): FacebookAuthenticationService => {
+  const jwtTokenGenerator = new JwtTokenGenerator(env.jwtSecret)
+  return new FacebookAuthenticationService(
+    makeFacebookApi(),
+    makePgUserAccountRepo(),
+    makeJwtTokenGenerator()
+  )
+}
