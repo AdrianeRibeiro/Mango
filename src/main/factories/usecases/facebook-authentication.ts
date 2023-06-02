6390211464179e919f42db0aeedd2740a0d33300
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-06-02 08:22:17 -0300

    renomeando arquivo

--- src/main/factories/usecases/facebook-authentication.ts
+++ src/main/factories/usecases/facebook-authentication.ts
@@ -1,15 +1,14 @@
 import { setupFacebookAuthentication, FacebookAuthentication } from "@/domain/usecases"
 import { env } from "@/main/config/env"
-import { JwtTokenGenerator } from "@/infra/crypto"
+import { JwtTokenHandler } from "@/infra/crypto"
 import { makeFacebookApi } from "@/main/factories/apis"
 import { makePgUserAccountRepo } from "@/main/factories/repos"
-import { makeJwtTokenGenerator } from "@/main/factories/crypto"
+import { makeJwtTokenHandler } from "@/main/factories/crypto"
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
-  const jwtTokenGenerator = new JwtTokenGenerator(env.jwtSecret)
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   )
 }
