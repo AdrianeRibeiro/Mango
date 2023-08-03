361a13680606a325b77e7f3c085c295316abc51d
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-08-03 08:40:48 -0300

    ensure ChangeProfilePicture calls DeleteFile if file exists and SaveUserPicture throws

--- src/domain/usecases/change-profile-picture.ts
+++ src/domain/usecases/change-profile-picture.ts
@@ -1,24 +1,30 @@
-import { UUIDGenerator, UploadFile } from "@/domain/contracts/gateways"
+import { UUIDGenerator, UploadFile, DeleteFile } from "@/domain/contracts/gateways"
 import { SaveUserPicture, LoadUserProfile } from "@/domain/contracts/repos"
 import { UserProfile } from "@/domain/entities"
 
-type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture & LoadUserProfile) => ChangeProfilePicture
+type Setup = (fileStorage: UploadFile & DeleteFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture & LoadUserProfile) => ChangeProfilePicture
 type Input = { id: string, file?: Buffer }
 type Output = { pictureUrl?: string, initials?: string }
 export type ChangeProfilePicture = (input: Input) => Promise<Output>
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepo) => async ({ id, file }) => {
+  const key = crypto.uuid({ key: id })
   const data: { pictureUrl?: string, name?: string } = {}
 
   if(file !== undefined) {
-    data.pictureUrl = await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
+    data.pictureUrl = await fileStorage.upload({ file, key })
   } else {
     data.name = (await userProfileRepo.load({ id })).name
   }
 
   const userProfile = new UserProfile(id)
   userProfile.setPicture(data)
-  await userProfileRepo.savePicture(userProfile)
+
+  try {
+    await userProfileRepo.savePicture(userProfile)
+  } catch {
+    await fileStorage.delete({ key })
+  }
 
   return userProfile
 }
