2328f3618b8ef70f5019bfee9f63ed3a1b182208
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-08-21 08:41:44 -0300

    move UUIDHandler to its own file

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -1,18 +1,16 @@
-import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { UUIDHandler } from '@/infra/crypto'
 import { v4 } from 'uuid'
 
 jest.mock('uuid')
 
-class UUIDHandler {
-  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
-    return `${key}_${v4()}`
-  }
-}
-
 describe('UUIDHandler', () => {
-  it('should call uuid.v4', () => {
-    const sut = new UUIDHandler()
+  let sut: UUIDHandler
 
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+
+  it('should call uuid.v4', () => {
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
@@ -20,14 +18,9 @@ describe('UUIDHandler', () => {
 
   it('should return correct uuid', () => {
     //mocked(v4).mockReturnValueOnce('any_uuid')
-    const sut = new UUIDHandler()
 
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toBe('any_key_any_uuid')
   })
 })
-function mocked(v4: (<T extends ArrayLike<number>>(options: import("uuid").V4Options | null | undefined, buffer: T, offset?: number | undefined) => T) & ((options?: import("uuid").V4Options | undefined) => string)) {
-  throw new Error('Function not implemented.')
-}
-
