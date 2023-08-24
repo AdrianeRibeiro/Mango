5092f4f5b7d9c1e808fca9db62f30b59acebd373
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-08-24 08:55:49 -0300

    save picture when user exists and not exists

--- tests/infra/repos/postgres/user-profile.spec.ts
+++ tests/infra/repos/postgres/user-profile.spec.ts
@@ -35,4 +35,20 @@ describe('PgUserProfileRepository', () => {
       expect(pgUser).toMatchObject({ id, pictureUrl: 'any_url', initials: null })
     })
   })
+
+  describe('savePicture', () => {
+    it('should load user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any_name' })
+
+      const userProfile = await sut.load({ id: id.toString() })
+
+      expect(userProfile?.name).toBe('any_name')
+    })
+
+    it('should return undefined', async () => {
+      const userProfile = await sut.load({ id: '99999' })
+
+      expect(userProfile).toBeUndefined()
+    })
+  })
 })
