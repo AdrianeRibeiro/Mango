05f6b26350911960300996dd95e41d0aa61db8c6
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-09-04 08:17:58 -0300

    move DeletePictureController to its own file

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,19 +1,4 @@
-import { Controller } from "@/application/controllers"
-import { ChangeProfilePicture } from "@/domain/usecases"
-import { HttpResponse, noContent } from "@/application/helpers"
-
-type HttpRequest = { userId: string }
-
-class DeletePictureController extends Controller {
-  constructor(private readonly changeProfilePicture: ChangeProfilePicture) {
-    super()
-  }
-
-  async perform ({ userId }: HttpRequest): Promise<HttpResponse> {
-    await this.changeProfilePicture({ id: userId })
-    return noContent()
-  }
-}
+import { Controller, DeletePictureController } from "@/application/controllers"
 
 describe('DeletePictureController', () => {
   let changeProfilePicture: jest.Mock
