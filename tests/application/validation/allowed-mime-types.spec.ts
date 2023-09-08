e4ffbade1241bcd4fb0ed4e0c6fa4be140f0bb13
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-09-08 10:03:46 -0300

    move code to source

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -1,29 +1,5 @@
 import { InvalidMymeTypeError } from "@/application/errors"
-
-type Extension = 'png' | 'jpg'
-
-class AllowedMimeTypes {
-  constructor(
-    private readonly allowed: Extension[],
-    private readonly mimeType: string
-  ) {}
-
-  validate(): Error | undefined {
-    let isValid = false
-    if (this.isPng()) isValid = true
-    else if (this.isJpg()) isValid = true
-
-    if(!isValid) return new InvalidMymeTypeError(this.allowed)
-  }
-
-  private isPng(): boolean {
-    return this.allowed.includes('png') && this.mimeType === 'image/png'
-  }
-
-  private isJpg(): boolean {
-    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimeType)
-  }
-}
+import { AllowedMimeTypes } from "@/application/validation"
 
 describe('AllowedMimeTypes', () => {
   it('should return InvalidMymeTypeError if value is invalid', () => {
