b166372b01ec3b74e0b8db9cd6a3d7d9fa7818d0
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-10-04 09:02:35 -0300

    ensure PgConnection creates a new connection

--- tests/infra/repos/postgres/helpers/connection.spec.ts
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -1,12 +1,29 @@
+import { createConnection, getConnectionManager } from "typeorm"
+
+jest.mock('typeorm', () => ({
+  Entity: jest.fn(),
+  PrimaryGeneratedColumn: jest.fn(),
+  Column: jest.fn(),
+  createConnection: jest.fn(),
+  getConnectionManager: jest.fn()
+}))
+
 class PgConnection {
   private static instance?: PgConnection
 
   private constructor() {}
 
   static getInstance (): PgConnection {
-    if(PgConnection.instance === undefined) PgConnection.instance == new PgConnection()
+    if(PgConnection.instance == undefined) {
+      PgConnection.instance == new PgConnection()
+    }
 
     return PgConnection.instance
+  }
+
+  async connect (): Promise<void> {
+    const connection = await createConnection()
+    connection.createQueryRunner()
   }
 }
 
@@ -16,5 +33,30 @@ describe('PgConnection', () => {
     const sut2 = PgConnection.getInstance()
 
     expect(sut).toBe(sut2)
+  })
+
+  it('should create a new connection', async () => {
+    const getConnectionManagerSpy = jest.fn().mockReturnValueOnce({
+      has: jest.fn().mockReturnValueOnce(false)
+    })
+    jest.fn().mockImplementationOnce(getConnectionManagerSpy)
+    //mocked(getConnectionManager).mockImplementationOnce(getConnectionManagerSpy)
+
+    const createQueryRunnerSpy = jest.fn()
+    const createConnectionSpy = jest.fn().mockResolvedValueOnce({
+      createQueryRunner: jest.fn()
+    })
+    jest.fn().mockImplementationOnce(getConnectionManagerSpy)
+    //mocked(createConnection).mockImplementationOnce(createConnectionSpy)
+
+    const sut = PgConnection.getInstance()
+
+    await sut.connect()
+
+    expect(createConnectionSpy).toHaveBeenCalledWith()
+    expect(createConnectionSpy).toHaveBeenCalledTimes(1)
+
+    expect(createQueryRunnerSpy).toHaveBeenCalledWith()
+    expect(createQueryRunnerSpy).toHaveBeenCalledTimes(1)
   })
 })
